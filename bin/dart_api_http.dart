f31ce3ce6d532ab4838a8173392178f543e974c6
luizmineiro <jluizmineiro@gmail.com>
2022-11-30 08:25:29 -0300

    toJson

--- bin/dart_api_http.dart
+++ bin/dart_api_http.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 Future main() async {
   final todo = await fetch();
-  print(todo.title);
+  print(todo.toJson());
 }
 
 Future<Todo> fetch() async {
-  var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/20');
+  var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
   var response = await http.get(url);
   // print(response.body);
   var json = jsonDecode(response.body); //transforma a string de json em um map
@@ -28,13 +27,24 @@ class Todo {
     required this.userId,
     required this.completed,
   });
-
-  factory Todo.fromJson(Map json) {
+  
+  //mapear json
+  factory Todo.fromJson(Map<String, dynamic> json) {
     return Todo(
       title: json['title'],
       id: json['id'],
       userId: json['userId'],
       completed: json['completed'],
     );
+  }
+
+  //retorna json
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'userId': userId,
+      'completed': completed,
+    };
   }
 }
